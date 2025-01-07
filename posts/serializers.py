0724acf4f983e593465660d581d950d5916ed806
Parsa <pheydari33@yahoo.com>
2025-01-07 18:15:31 +0330

    like and comment added.

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Post
+from .models import Post, Comment, Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -9,4 +9,25 @@ class PostSerializer(serializers.ModelSerializer):
         fields = ('user', 'title', 'caption', 'is_active', 'is_public')
         extra_kwargs = {
             'user': {'read_only': True}
+        }
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('post', 'user', 'text')
+        extra_kwargs = {
+            'post': {'read_only': True},
+            'user': {'read_only': True}
+        }
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ('post', 'user', 'is_liked')
+        extra_kwargs = {
+            'post': {'read_only': True},
+            'user': {'read_only': True},
+            'is_liked': {'required': False}
         }
--- posts/views.py
+++ posts/views.py
@@ -3,8 +3,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Post, PostFile
-from .serializers import PostSerializer
+from .models import Post, Like, Comment
+from .serializers import PostSerializer, CommentSerializer, LikeSerializer
 
 
 
@@ -55,10 +55,50 @@ class CommentView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         
 
-        # comments = Comment.objects.filter(post=post, is_proved=True)
+        # comments = Comment.objects.filter(post=post, is_approved=True)
         comments = post.comments.filter(is_approved=True)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
 
+
+    def post(self, request, post_pk):
+        post = self. get_post(post_pk)
+        if not post:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(post=post, user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LikeView(APIView):
-    pass
+    permission_classes = [IsAuthenticated]
+
+    def get_post(self, post_pk):
+        try:
+            return Post.objects.get(pk=post_pk)
+        except Post.DoesNotExist:
+            return False
+
+
+    def get(self, request, post_pk):
+        post = self.get_post(post_pk)
+        if not post: 
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        likes = post.likes.filter(is_liked=True).count()
+        return Response({'likes': likes})
+
+
+    def post(self, request, post_pk):
+        post = self. get_post(post_pk)
+        if not post: 
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = LikeSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(post=post, user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
